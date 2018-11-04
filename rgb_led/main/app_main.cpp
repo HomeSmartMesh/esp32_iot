4fb905ce9c8ae6db4f48d9479f045a7eb8e851e4
wassfila <wassim.filali@gmail.com>
2018-11-04 16:16:57 +0100

    running mqtt cleaning and fixed c to c++ allocation issues

--- rgb_led/main/app_main.cpp
+++ rgb_led/main/app_main.cpp
@@ -31,28 +31,6 @@ bool is_client_ready = false;
 const gpio_num_t BLUE_LED=(gpio_num_t)2;
 const gpio_num_t HEATER_GPIO=(gpio_num_t)15;
 
-int heat_request = 0;
-int heat_timer = 0;
-
-int atoi_n(const char * str,int len)
-{
-    char str_end_0[11];
-    if(len > 10)
-    {
-        len = 10;
-    }
-    memcpy(str_end_0,str,len);
-    str_end_0[len] = 0;
-    return atoi(str_end_0);
-}
-
-int set_heat_1h(const char * payload,int len)
-{
-    heat_request = atoi_n(payload,len);
-    heat_timer = 6*60;// 1h
-    return heat_request;
-}
-
 static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 {
     g_client = event->client;//update g_client
@@ -62,11 +40,8 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
         case MQTT_EVENT_CONNECTED:
             is_client_ready = true;
             ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-            msg_id = esp_mqtt_client_publish(g_client, "esp/bed heater/status", "online", 0, 1, 1);
+            msg_id = esp_mqtt_client_publish(g_client, "esp/rgb led/status", "online", 0, 1, 1);
             ESP_LOGI(TAG, "MQTT> sent publish successful, msg_id=%d", msg_id);
-
-            msg_id = esp_mqtt_client_subscribe(g_client, "esp/bed heater/1h", 0);
-            ESP_LOGI(TAG, "MQTT> sent subscribe successful, msg_id=%d", msg_id);
 
             break;
         case MQTT_EVENT_DISCONNECTED:
@@ -86,15 +61,6 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
             ESP_LOGI(TAG, "MQTT_EVENT_DATA");
             printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
             printf("DATA=%.*s\r\n", event->data_len, event->data);
-            if(strncmp(event->topic,"esp/bed heater/1h",event->topic_len) == 0)
-            {
-                int heat = set_heat_1h(event->data,event->data_len);
-                ESP_LOGI(TAG, "MQTT> heat request 1h at %d", heat);
-            }
-            else
-            {
-                ESP_LOGI(TAG, "MQTT> unhandled topic %s", event->topic);
-            }
             break;
         case MQTT_EVENT_ERROR:
             ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
@@ -132,6 +98,7 @@ static void wifi_init(void)
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
     wifi_config_t wifi_config;
+    memset(&wifi_config,0,sizeof(wifi_config_t));
     strcpy((char*)wifi_config.sta.ssid,CONFIG_WIFI_SSID);
     strcpy((char*)wifi_config.sta.password,CONFIG_WIFI_PASSWORD);
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
@@ -142,10 +109,14 @@ static void wifi_init(void)
     xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
 }
 
+char mqtt_uri[32];
+
 static void mqtt_app_start(void)
 {
     esp_mqtt_client_config_t mqtt_cfg;
-    mqtt_cfg.uri = CONFIG_BROKER_URL;
+    memset(&mqtt_cfg,0,sizeof(esp_mqtt_client_config_t));
+    strcpy(mqtt_uri,CONFIG_BROKER_URL);
+    mqtt_cfg.uri = mqtt_uri;
     mqtt_cfg.event_handle = mqtt_event_handler;
 
     g_client = esp_mqtt_client_init(&mqtt_cfg);
@@ -158,10 +129,10 @@ void publish_heat_status(int heat,int timer)
     {
         char payload[10];
         sprintf(payload,"%d",heat);
-        int msg_id = esp_mqtt_client_publish(g_client, "esp/bed heater/heating", payload, 0, 1, 0);
+        int msg_id = esp_mqtt_client_publish(g_client, "esp/rgb led/heating", payload, 0, 1, 0);
         ESP_LOGD(TAG, "heater> sent publish successful, msg_id=%d", msg_id);
         sprintf(payload,"%d",timer);
-        msg_id = esp_mqtt_client_publish(g_client, "esp/bed heater/timer", payload, 0, 1, 0);
+        msg_id = esp_mqtt_client_publish(g_client, "esp/rgb led/timer", payload, 0, 1, 0);
         ESP_LOGD(TAG, "heater> sent publish successful, msg_id=%d", msg_id);
     }
     else
@@ -170,49 +141,18 @@ void publish_heat_status(int heat,int timer)
     }
 }
 
-void heater_gpio_task(void *pvParameter)
+void rgb_gpio_task(void *pvParameter)
 {
     gpio_pad_select_gpio(BLUE_LED);
     gpio_pad_select_gpio(HEATER_GPIO);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(HEATER_GPIO, GPIO_MODE_OUTPUT);
     gpio_set_direction(BLUE_LED, GPIO_MODE_OUTPUT);
-    static int loop_i=0;
     while(1) {
-        ESP_LOGI(TAG, "heater> loop %d", loop_i++);
-        if( (heat_request > 0) && (heat_timer > 0) )
-        {
-            int heat_value = heat_request;
-            if(heat_value > 10)
-            {
-                heat_value = 10;
-            }
-            publish_heat_status(heat_value,heat_timer);
-            int rest_value = 10 - heat_value;
-            gpio_set_level(HEATER_GPIO, 1);
-            gpio_set_level(BLUE_LED, 1);
-            ESP_LOGI(TAG, "heater> Up on");
-            vTaskDelay(heat_value * 1000 / portTICK_PERIOD_MS);
-            gpio_set_level(HEATER_GPIO, 0);
-            gpio_set_level(BLUE_LED, 0);
-            ESP_LOGI(TAG, "heater> Down off");
-            vTaskDelay(rest_value * 1000 / portTICK_PERIOD_MS);
-
-            heat_timer--;
-        }
-        else
-        {
-            gpio_set_level(HEATER_GPIO, 0);
-            gpio_set_level(BLUE_LED, 0);
-            ESP_LOGI(TAG, "heater> IDLE Down off");
-            static int idle_loop_i = 0;
-            if((idle_loop_i++ % 20) ==0)
-            {
-                publish_heat_status(0,0);
-            }
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-        
+        gpio_set_level(BLUE_LED, 1);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        gpio_set_level(BLUE_LED, 0);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
 
@@ -234,7 +174,7 @@ void app_main()
     nvs_flash_init();
     wifi_init();
 
-    xTaskCreate(&heater_gpio_task, "heater_gpio_task", 2048, NULL, 5, NULL);
+    xTaskCreate(&rgb_gpio_task, "rgb_gpio_task", 2048, NULL, 5, NULL);
 
     mqtt_app_start();
 }
